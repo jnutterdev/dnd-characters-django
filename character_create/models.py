c5b4b0c841ece0ce02d578fcada340d12d464d42
jnutterdev <hello@jnutterdev.com>
2022-12-19 15:55:40 -0500

    Implementing image upload in admin

--- character_create/models.py
+++ character_create/models.py
@@ -2,6 +2,7 @@ from django.db import models
 
 # Create your models here.
 class Character(models.Model):
+    character_image = models.ImageField(upload_to='images/', blank=True, null=True)
     name = models.CharField(max_length=250)
     species = models.CharField(max_length=250)
     char_class = models.CharField(max_length=250)
