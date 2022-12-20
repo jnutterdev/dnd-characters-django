0e2262719cd68e70df59dbae8da2c5a64fe9e2fd
jnutterdev <hello@jnutterdev.com>
2022-12-20 09:25:14 -0500

    Added user avatar and updated settings

--- dnd_project/settings.py
+++ dnd_project/settings.py
@@ -21,9 +21,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 MEDIA_URL = "/media/"
 MEDIA_ROOT = BASE_DIR / "media"
 
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
-
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.getenv("DJANGO_SECRET_KEY")
 
